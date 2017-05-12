fc1056a479ecdcc008c0e7cdebf6b21c2907268d
Tipok <a.tymofiev@gmail.com>
2017-05-12 13:57:18 +0300

    c2

--- src/controller/CourierController.java
+++ src/controller/CourierController.java
@@ -5,9 +5,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
 import javax.swing.JTable;
 import javax.swing.event.TableModelListener;
 import javax.swing.table.AbstractTableModel;
+import model.Car;
 import view.MainView;
 import model.CourierStatus;
 
@@ -19,13 +22,39 @@ public class CourierController {
         connection = DataBaseConnection.getConnection();
     }
 
-    public String[] getFreeCarsID(String partOfName) {
+    public Car[] getCars() {
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select cars.carsID, cars.carsName from cars\n"
+                    + "left outer join couriers\n"
+                    + "on cars.carsID = couriers.carsID\n"
+                    + "where couriers.carsID is null\n"
+                    + "order by cars.carsID desc");
+
+            int n = 0;
+            while (resultSet.next()) {
+                n++;
+            }
+            resultSet.first();
+            resultSet.previous();
+            Car[] cars = new Car[n];
+            n--;
+            while (resultSet.next()) {
+                cars[n--] = new Car(resultSet.getString("carsID"), resultSet.getString("carsName"));
+            }
+            return cars;
+        } catch (Exception ex) {
+            return null;
+        }
+    }
+
+    public String[] getFreeCarsID(String partOfID) {
         try {
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("select cars.carsID from cars\n"
                     + "left outer join couriers\n"
                     + "on cars.carsID = couriers.carsID\n"
-                    + "where couriers.carsID is null and cars.carsName like \'%" + partOfName + "%\'\n"
+                    + "where couriers.carsID is null and cars.carsID like \'%" + partOfID + "%\'\n"
                     + "order by cars.carsID desc");
 
             int n = 0;
@@ -45,13 +74,13 @@ public class CourierController {
         }
     }
 
-    public String[] getFreeCarsName(String ID) {
+    public String[] getFreeCarsName(String partOfName) {
         try {
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("select cars.carsName from cars\n"
                     + "left outer join couriers\n"
                     + "on cars.carsID = couriers.carsID\n"
-                    + "where couriers.carsID is null and cars.carsID like \'%" + ID + "%\'\n"
+                    + "where couriers.carsID is null and cars.carsName like \'%" + partOfName + "%\'\n"
                     + "order by cars.carsName desc");
 
             int n = 0;
@@ -71,17 +100,70 @@ public class CourierController {
         }
     }
 
-    public void addCourier(String PIP, int ID) {
+    public String[] getCarsForID(String ID) {
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select cars.carsName from cars\n"
+                    + "left outer join couriers\n"
+                    + "on cars.carsID = couriers.carsID\n"
+                    + "where couriers.carsID is null and cars.carsID = " + ID + "\n"
+                    + "order by cars.carsName desc");
+
+            int n = 0;
+            while (resultSet.next()) {
+                n++;
+            }
+            resultSet.first();
+            resultSet.previous();
+            String[] result = new String[n];
+            n--;
+            while (resultSet.next()) {
+                result[n--] = resultSet.getString("carsName");
+            }
+            return result;
+        } catch (Exception ex) {
+            return new String[]{"Error"};
+        }
+    }
+
+    public String[] getCarsForName(String name) {
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select cars.carsID from cars\n"
+                    + "left outer join couriers\n"
+                    + "on cars.carsID = couriers.carsID\n"
+                    + "where couriers.carsID is null and cars.carsName = " + name + "\n"
+                    + "order by cars.carsID desc");
+
+            int n = 0;
+            while (resultSet.next()) {
+                n++;
+            }
+            resultSet.first();
+            resultSet.previous();
+            String[] result = new String[n];
+            n--;
+            while (resultSet.next()) {
+                result[n--] = resultSet.getString("carsID");
+            }
+            return result;
+        } catch (Exception ex) {
+            return new String[]{"Error"};
+        }
+    }
+
+    public void addCourier(String PIP, String phone, int carID) {
         new Thread() {
             @Override
             public void run() {
                 try {
                     PreparedStatement preparedStatement;
                     preparedStatement = connection.prepareStatement(
-                            "insert into couriers values(null, ?, ?, ?)");
+                            "insert into couriers values(null, ?, ?, ?, ?)");
                     preparedStatement.setString(1, PIP);
-                    preparedStatement.setInt(2, ID);
-                    preparedStatement.setString(3, CourierStatus.UNKNOWN);
+                    preparedStatement.setString(2, phone);
+                    preparedStatement.setInt(3, carID);
+                    preparedStatement.setString(4, CourierStatus.UNKNOWN);
                     preparedStatement.executeUpdate();
                 } catch (Exception ex) {
                     MainView.showErrorPane("Не вдалось додати запис у БД.", ex);
@@ -94,11 +176,12 @@ public class CourierController {
         try {
             table.setModel(new CourierTableModel());
             table.getColumnModel().getColumn(0).setMaxWidth(50);
-            table.getColumnModel().getColumn(1).setMinWidth(180);
-            table.getColumnModel().getColumn(2).setMinWidth(100);
-            table.getColumnModel().getColumn(2).setMaxWidth(120);
+            table.getColumnModel().getColumn(1).setMinWidth(120);
+            table.getColumnModel().getColumn(2).setMinWidth(80);
             table.getColumnModel().getColumn(3).setMinWidth(100);
-            table.getColumnModel().getColumn(3).setMaxWidth(100);
+            table.getColumnModel().getColumn(3).setMaxWidth(120);
+            table.getColumnModel().getColumn(4).setMinWidth(100);
+            table.getColumnModel().getColumn(4).setMaxWidth(100);
             table.updateUI();
 
         } catch (Exception ex) {
@@ -135,7 +218,7 @@ public class CourierController {
 
         @Override
         public int getColumnCount() {
-            return 4;
+            return 5;
         }
 
         @Override
@@ -146,8 +229,10 @@ public class CourierController {
                 case 1:
                     return "ПІП";
                 case 2:
-                    return "Транспорт (ID)";
+                    return "Телефон";
                 case 3:
+                    return "Транспорт (ID)";
+                case 4:
                     return "Статус";
                 default:
                     return "";
@@ -177,8 +262,10 @@ public class CourierController {
                     case 1:
                         return resultSet.getString("couriersName");
                     case 2:
-                        return resultSet.getString("carsID");
+                        return resultSet.getString("couriersPhone");
                     case 3:
+                        return resultSet.getString("carsID");
+                    case 4:
                         return resultSet.getString("couriersStatus");
                     default:
                         return "Error";
--- src/view/AddCourierView.java
+++ src/view/AddCourierView.java
@@ -11,6 +11,9 @@ import model.DataBaseConnection;
 import controller.CourierController;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.util.HashMap;
+import java.util.Map;
+import model.Car;
 
 /**
  *
@@ -23,7 +26,7 @@ public class AddCourierView extends javax.swing.JInternalFrame implements Runnab
      */
     private static AddCourierView addCourierView;
     private CourierController courierController;
-    private static boolean mutex = false;
+    private Car[] cars;
 
     private AddCourierView(CourierController courierController) {
         initComponents();
@@ -31,43 +34,42 @@ public class AddCourierView extends javax.swing.JInternalFrame implements Runnab
         setClosable(true);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setIconifiable(true);
+
         jComboBox1.removeAllItems();
         jComboBox2.removeAllItems();
+        cars = courierController.getCars();
+
+        for (Car car : cars) {
+            jComboBox1.addItem(car.getCarID());
+            jComboBox2.addItem(car.getCarName());
+        }
 
         ////розібратись шоби пахало
-//        jComboBox1.addItemListener(new ItemListener() {
-//            @Override
-//            public void itemStateChanged(ItemEvent e) {
-//
-//                if (mutex) {
-//                    jComboBox2.removeAllItems();
-//                    for (String name : courierController.getFreeCarsName(e.getItem().toString())) {
-//                        jComboBox2.addItem(name);
-//                    }
-//                }
-//                mutex = true;
-//            }
-//        });
-//
-//        jComboBox2.addItemListener(new ItemListener() {
-//            @Override
-//            public void itemStateChanged(ItemEvent e) {
-//                if (!mutex) {
-//                    jComboBox1.removeAllItems();
-//                    for (String id : courierController.getFreeCarsID(e.getItem().toString())) {
-//                        jComboBox1.addItem(id);
-//                    }
-//                }
-//                mutex = false;
-//            }
-//        });
-        for (String id : courierController.getFreeCarsID("")) {
-            jComboBox1.addItem(id);
+        jComboBox1.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                for (Car car : cars) {
+                    if (car.getCarID().equals(e.getItem())) {
+                        jComboBox2.setSelectedItem(car.getCarName());
+                    }
+                }
+            }
         }
+        );
 
-        for (String name : courierController.getFreeCarsName("")) {
-            jComboBox2.addItem(name);
+        jComboBox2.addItemListener(
+                new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e
+            ) {
+                for (Car car : cars) {
+                    if (car.getCarName().equals(e.getItem())) {
+                        jComboBox2.setSelectedItem(car.getCarID());
+                    }
+                }
+            }
         }
+        );
     }
 
     public static AddCourierView getAddCourierView(CourierController courierController) {
@@ -91,6 +93,8 @@ public class AddCourierView extends javax.swing.JInternalFrame implements Runnab
         jComboBox1 = new javax.swing.JComboBox<>();
         jLabel7 = new javax.swing.JLabel();
         jComboBox2 = new javax.swing.JComboBox<>();
+        jTextField3 = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Новий кур'єр");
@@ -122,44 +126,53 @@ public class AddCourierView extends javax.swing.JInternalFrame implements Runnab
         jComboBox2.setEditable(true);
         jComboBox2.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
 
+        jLabel2.setText("Тел.");
+        jLabel2.setToolTipText("");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel7, javax.swing.GroupLayout.DEFAULT_SIZE, 255, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jTextField2))
+                    .addComponent(jLabel6, javax.swing.GroupLayout.DEFAULT_SIZE, 255, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel7, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jLabel6, javax.swing.GroupLayout.DEFAULT_SIZE, 255, Short.MAX_VALUE)
-                            .addGroup(layout.createSequentialGroup()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                    .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jComboBox1, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(jComboBox2, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jTextField2))))
+                            .addComponent(jComboBox1, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jComboBox2, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(99, 99, 99)
-                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(93, 93, 93)
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jTextField3)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jLabel7, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(13, 13, 13)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -170,8 +183,7 @@ public class AddCourierView extends javax.swing.JInternalFrame implements Runnab
                     .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton1)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(jButton1))
         );
 
         pack();
@@ -181,6 +193,7 @@ public class AddCourierView extends javax.swing.JInternalFrame implements Runnab
         boolean error = false;
         try {
             String PIP;
+            String phone;
             int carID = -1;
 
             jTextField2.setBackground(Color.white);
@@ -192,16 +205,21 @@ public class AddCourierView extends javax.swing.JInternalFrame implements Runnab
             }
 
             PIP = jTextField2.getText();
+            phone = jTextField3.getText();
 
             if (PIP.length() < 3) {
                 jTextField2.setBackground(Color.red);
+                error = true;
+            }
+            if (phone.length() < 5) {
+                jTextField3.setBackground(Color.red);
                 error = true;
             }
             if (error) {
                 throw new Exception();
             }
 
-            courierController.addCourier(PIP, carID);
+            courierController.addCourier(PIP, phone, carID);
 
             this.dispose();
         } catch (Exception ex) {
@@ -214,11 +232,13 @@ public class AddCourierView extends javax.swing.JInternalFrame implements Runnab
     private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JComboBox<String> jComboBox2;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField jTextField3;
     // End of variables declaration//GEN-END:variables
 
     @Override
